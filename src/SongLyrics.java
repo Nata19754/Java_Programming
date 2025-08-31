f66221855cbe8392d0d0f2c789411ce1ecf1436f
Nata19754 <chavesnatally0419@gmail.com>
2025-08-30 23:42:44 -0400

    Java programming SongLyrics Assignment

---
+++ src/SongLyrics.java
@@ -0,0 +1,14 @@
+public class SongLyrics
+{
+    public static void main(String[] args){
+        // Lyric line 1
+    System.out.println("Got a fire in my soul");
+        // Lyric line 2
+    System.out.println("I've lost faith in this broken system");
+        // Lyric line 3
+    System.out.println("Got love for my home");
+        // Lyric line 4
+    System.out.println("But if we cry is there anyone listening");
+        // Song : Renegades By: ONE OK ROCK
+    }
+}
